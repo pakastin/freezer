3a413c738431511c562c185b1815c1e675cc79da
Juha Lindstedt <juha@pakastin.fi>
2016-01-03 02:48:05 +0200

    more interesting examples and more magic to codeoutput.js

--- js/codeoutput.js
+++ js/codeoutput.js
@@ -1,17 +1,37 @@
 function codeOutput () {
   var $container = document.getElementById('container');
-  var html = "<!DOCTYPE html><html><head><style>body{font-family:'Source Sans Pro', sans-serif;} h1{font-weight:400;}</style><link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'></head><body><script>";
+  var html = [
+    "<!DOCTYPE html>",
+    "<html>",
+    " <head>",
+    "  <style>",
+    "   body {",
+    "    font-family:'Source Sans Pro', sans-serif;",
+    "   }",
+    "   h1 {",
+    "    font-weight:600;",
+    "   }",
+    "  </style>",
+    "  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,600' rel='stylesheet' type='text/css'>",
+    " </head>",
+    "<body>",
+    " <script>"
+  ];
 
   for (var i = 0; i < arguments.length; i++) {
-    html += document.getElementsByTagName('pre')[arguments[i]].firstChild.textContent;
+    html.push(document.getElementsByTagName('pre')[arguments[i]].firstChild.textContent);
   };
 
   var afterEl = document.getElementsByTagName('pre')[arguments[i - 1]].nextSibling;
 
-  html += "<\/script><\/body><\/html>";
+  html.push(
+    "  <\/script>",
+    " <\/body>",
+    "<\/html>"
+  );
 
   var a = document.createElement('a');
-  a.href = 'data:text/html;charset=utf-8,' + encodeURI(html);
+  a.href = 'data:text/html;charset=utf-8,' + encodeURI(html.join('\n'));
   a.target = '_blank';
   a.textContent = 'Run result';
   container.insertBefore(a, afterEl.nextSibling);
